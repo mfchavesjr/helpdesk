26ea867715dbf503874192bb4885981b8b5bb683
mfchavesjr <feira.nova@gmail.com>
2021-11-30 14:30:34 -0300

    Criação das anotações JPA para as classes de dominio

--- src/main/java/com/mfchaves/helpdesk/domain/Cliente.java
+++ src/main/java/com/mfchaves/helpdesk/domain/Cliente.java
@@ -3,12 +3,22 @@ package com.mfchaves.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.mfchaves.helpdesk.domain.enums.Perfil;
+
+@Entity
 public class Cliente extends Pessoa {
 
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
