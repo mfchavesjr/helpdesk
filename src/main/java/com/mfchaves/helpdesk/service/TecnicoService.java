e1bc6e66ef100cd850ef9cab0463223b5722b4c0
mfchavesjr <feira.nova@gmail.com>
2021-11-30 23:38:18 -0300

    Criação do Controller e Service do Tecnico

---
+++ src/main/java/com/mfchaves/helpdesk/service/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.mfchaves.helpdesk.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mfchaves.helpdesk.domain.Tecnico;
+import com.mfchaves.helpdesk.repository.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> optional = tecnicoRepository.findById(id);
+		return optional.orElse(null);
+	}
+}
