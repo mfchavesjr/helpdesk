820dd934423eefbb42c3114aa132af25b96d5e02
mfchavesjr <feira.nova@gmail.com>
2021-12-01 09:32:36 -0300

    Endpoint ListAll

--- src/main/java/com/mfchaves/helpdesk/controller/TecnicoController.java
+++ src/main/java/com/mfchaves/helpdesk/controller/TecnicoController.java
@@ -1,5 +1,8 @@
 package com.mfchaves.helpdesk.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +17,7 @@ import com.mfchaves.helpdesk.service.TecnicoService;
 @RestController
 @RequestMapping(value = "/tecnico")
 public class TecnicoController {
-	
+
 	@Autowired
 	private TecnicoService tecnicoService;
 
@@ -22,6 +25,13 @@ public class TecnicoController {
 	public ResponseEntity<TecnicoDto> findById(@PathVariable Integer id) {
 		Tecnico tecnico = tecnicoService.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDto(tecnico));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<TecnicoDto>> findAll(){
+		List<Tecnico> list = tecnicoService.findAll();
+		List<TecnicoDto> listDto = list.stream().map(obj -> new TecnicoDto(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
--- src/main/java/com/mfchaves/helpdesk/service/TecnicoService.java
+++ src/main/java/com/mfchaves/helpdesk/service/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.mfchaves.helpdesk.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> optional = tecnicoRepository.findById(id);
 		return optional.orElseThrow(() -> new ObjectNotFoundException("Id inexistente: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return tecnicoRepository.findAll();
 	}
 }
