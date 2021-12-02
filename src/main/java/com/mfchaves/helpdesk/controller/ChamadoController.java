83572dc802169977e0de6cb4bb3d31845a1353f2
mfchavesjr <feira.nova@gmail.com>
2021-12-02 15:38:27 -0300

    Endpoint CREATE Chamados

--- src/main/java/com/mfchaves/helpdesk/controller/ChamadoController.java
+++ src/main/java/com/mfchaves/helpdesk/controller/ChamadoController.java
@@ -1,14 +1,20 @@
 package com.mfchaves.helpdesk.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mfchaves.helpdesk.domain.Chamado;
 import com.mfchaves.helpdesk.domain.dto.ChamadoDto;
@@ -33,6 +39,14 @@ public class ChamadoController {
 	public ResponseEntity<ChamadoDto> findById(@PathVariable Integer id) {
 		Chamado chamado = chamadoService.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDto(chamado));
+	}
+
+	@PostMapping
+	public ResponseEntity<ChamadoDto> create(@Valid @RequestBody ChamadoDto chamadoDto) {
+		Chamado chamado = chamadoService.create(chamadoDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(chamado.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/mfchaves/helpdesk/service/ChamadoService.java
+++ src/main/java/com/mfchaves/helpdesk/service/ChamadoService.java
@@ -3,26 +3,61 @@ package com.mfchaves.helpdesk.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mfchaves.helpdesk.domain.Chamado;
+import com.mfchaves.helpdesk.domain.Cliente;
+import com.mfchaves.helpdesk.domain.Tecnico;
+import com.mfchaves.helpdesk.domain.dto.ChamadoDto;
+import com.mfchaves.helpdesk.domain.enums.Prioridade;
+import com.mfchaves.helpdesk.domain.enums.Status;
 import com.mfchaves.helpdesk.exceptions.ObjectNotFoundException;
 import com.mfchaves.helpdesk.repository.ChamadoRepository;
 
 @Service
 public class ChamadoService {
-	
+
 	@Autowired
 	private ChamadoRepository chamadoRepository;
-	
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
+
 	public Chamado findById(Integer id) {
 		Optional<Chamado> chamado = chamadoRepository.findById(id);
 		return chamado.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado! ID: " + id));
 	}
-	
-	public List<Chamado> listAll(){
+
+	public List<Chamado> listAll() {
 		return chamadoRepository.findAll();
+	}
+
+	public Chamado create(@Valid ChamadoDto chamadoDto) {		
+		return chamadoRepository.save(newChamado(chamadoDto));
+	}
+
+	private Chamado newChamado(ChamadoDto chamadoDto) {
+		Tecnico tecnico = tecnicoService.findById(chamadoDto.getTecnico());
+		Cliente cliente = clienteService.findById(chamadoDto.getCliente());
+
+		Chamado chamado = new Chamado();
+
+		if (chamadoDto.getId() != null) {
+			chamado.setId(chamadoDto.getId());
+		}
+		chamado.setCliente(cliente);
+		chamado.setTecnico(tecnico);
+		chamado.setPrioridade(Prioridade.toEnum(chamadoDto.getPrioridade()));
+		chamado.setStatus(Status.toEnum(chamadoDto.getStatus()));
+		chamado.setTitulo(chamadoDto.getTitulo());
+		chamado.setObservacoes(chamadoDto.getObservacoes());
+		
+		return chamado;
+
 	}
 
 }
